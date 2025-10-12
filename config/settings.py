1d9c3311da309ab1c4eeafe7d4bc3d3cec2ba07c
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-10-13 03:01:15 +0800

    New database add postgreSQL

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+from decouple import config
+
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,24 +107,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER"),
-#         'PASSWORD': os.getenv("DB_PASSWORD"),
-#         'HOST': os.getenv("DB_HOST", "localhost"),
-#         'PORT': os.getenv("DB_PORT", "5432"),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 
+# DATABASES = {
+#     "default": dj_database_url.parse(config("DB_URL"),  conn_max_age=600)
+# }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DB_URL"))
+}
 
 
 # Password validation
