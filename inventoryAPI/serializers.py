68dcd451e4e089e933ae510fa4c021067fc8e5bd
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-10-18 20:19:17 +0800

    New changes

--- inventoryAPI/serializers.py
+++ inventoryAPI/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Inventory
+import os
+from cloudinary import utils
 
 class InventorySerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name', read_only=True)
@@ -11,8 +13,7 @@ class InventorySerializer(serializers.ModelSerializer):
         fields = ['id', 'product', 'product_name', 'size', 'price', 'stock', 'image', 'is_new','is_available']
     
     def get_image(self, obj):
-        request = self.context.get('request')  # Optional, if you want absolute URL
         if obj.image:
-            # If you have MEDIA_URL exposed
-            return f"https://momoy-api.onrender.com{obj.image.url}"
+            # Use Cloudinary URL directly
+            return obj.image.url
         return None
