5bd8e057def6bd8c861fe8e9f2927c4b6d7925d4
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-10-19 00:23:45 +0800

    bwisit

--- inventoryAPI/serializers.py
+++ inventoryAPI/serializers.py
@@ -12,14 +12,22 @@ class InventorySerializer(serializers.ModelSerializer):
         model = Inventory
         fields = ['id', 'product', 'product_name', 'size', 'price', 'stock', 'image', 'is_new','is_available']
 
+    def get_image(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.image.url)  # full URL
+            return obj.image.url  # fallback relative URL
+        return None
+
     # def get_image(self, obj):
     #     if obj.inventory.image:
     #         return f"https://momoy-api.onrender.com{obj.inventory.image.url}"
     #     return None
 
-    def get_image(self, obj):
-        if obj.image:
-            # Always return the full URL from the image field
-            # Cloudinary will handle production URLs automatically
-            return obj.image.url
-        return None
+    # def get_image(self, obj):
+    #     if obj.image:
+    #         # Always return the full URL from the image field
+    #         # Cloudinary will handle production URLs automatically
+    #         return obj.image.url
+    #     return None
