0f76db0939ec61d2114c7077ed07a2c1ebb81175
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-10-27 12:03:00 +0800

    Payment image

--- paymentAPI/models.py
+++ paymentAPI/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Payment(models.Model):
     PAYMENT_METHOD_CHOICES = [
@@ -15,7 +16,8 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-    proof_image = models.ImageField(upload_to='payments/', blank=True, null=True)  # For GCash proof of payment
+    # proof_image = models.ImageField(upload_to='payments/', blank=True, null=True)  # For GCash proof of payment
+    proof_image = CloudinaryField('image', folder='payments/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
