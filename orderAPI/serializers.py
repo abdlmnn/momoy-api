b7cd72ebdbb315a91d9029a41f09455972b28c33
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-10-28 07:46:07 +0800

    new added

--- orderAPI/serializers.py
+++ orderAPI/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Order
 from orderlineAPI.serializers import OrderlineSerializer
 from paymentAPI.serializers import PaymentSerializer
+from userAPI.models import UserAddress
 
 class OrderSerializer(serializers.ModelSerializer):
     user_email = serializers.CharField(source='user.email', read_only=True)
@@ -22,6 +23,18 @@ class OrderSerializer(serializers.ModelSerializer):
     #     return None
 
 class AdminOrderSerializer(OrderSerializer):
+    user_addresses = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
-        fields = ['id', 'user_email', 'total_amount', 'status', 'created_at', 'updated_at', 'orderlines', 'payment']
+        fields = ['id', 'user_email', 'total_amount', 'status', 'created_at', 'updated_at', 'orderlines', 'payment', 'user_addresses']
+
+    def get_user_addresses(self, obj):
+        addresses = UserAddress.objects.filter(user=obj.user)
+        return [{
+            'id': addr.id,
+            'address': addr.address,
+            'latitude': addr.latitude,
+            'longitude': addr.longitude,
+            'is_default': addr.is_default
+        } for addr in addresses]
